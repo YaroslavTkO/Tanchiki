df27fcb21d8bbe9991870fe29622f894f5517e78
YaroslavTk <yaroscro@gmail.com>
2023-03-14 20:15:27 +0200

    Added turret and turret controller

---
+++ Assets/Scripts/Tank/TankMovement.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankMovement : MonoBehaviour
+{
+    private GameObject tank;
+
+    readonly private float _speed = 3f;
+    readonly private float _rotationSpeed = 60f;
+    readonly private float backwardSpeedMultiplier = 0.5f;
+
+    private void MoveTank()
+    {
+
+        float input = Input.GetAxisRaw("Vertical");
+        if (input < 0)
+            input *= backwardSpeedMultiplier;
+
+        Vector3 movement = input * tank.transform.right * Time.deltaTime * _speed;
+        tank.transform.position += movement;
+
+    }
+
+    private void RotateTank()
+    {
+        float rotate = -Input.GetAxisRaw("Horizontal") * _rotationSpeed * Time.deltaTime;
+
+        tank.transform.Rotate(0, 0, rotate);
+    }
+
+    void Start()
+    {
+        tank = gameObject;
+    }
+
+    void Update()
+    {
+        MoveTank();
+        RotateTank();
+    }
+
+
+}
---
+++ Assets/Scripts/Tank/TurretController.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurretController : MonoBehaviour
+{
+    private GameObject turret;
+
+    readonly private float _rotationSpeed = 80f;
+
+    void Start()
+    {
+        turret = gameObject;
+        
+    }
+
+    void Update()
+    {
+        RotateTurret();
+    }
+
+    private void RotateTurret()
+    {
+        float rotateLeft = Input.GetKey(KeyCode.Z) ? _rotationSpeed * Time.deltaTime : 0;
+        float rotateRight = Input.GetKey(KeyCode.X) ?  -_rotationSpeed * Time.deltaTime : 0;
+        turret.transform.Rotate(0, 0, rotateLeft + rotateRight);
+    }
+}
