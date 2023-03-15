acfd7ccebc6e490db94a8e555136fd776cb88bfd
YaroslavTk <yaroscro@gmail.com>
2023-03-15 19:52:56 +0200

    added script that changes camera size to fit the map

---
+++ Assets/Scripts/Other/CameraScaler.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScaler : MonoBehaviour
+{
+    private Renderer objectToFitInCamera;
+
+    private Camera _camera;
+
+    private void Start()
+    {
+        _camera = Camera.main;
+        objectToFitInCamera = GetComponent<Renderer>();
+        ChangeCameraSize();
+    }
+
+    private void ChangeCameraSize()
+    {
+        var size = objectToFitInCamera.bounds.size;
+        var width = size.x;
+        var height = size.y;
+
+        _camera.orthographicSize = ((width > height * _camera.aspect) ? width / (float)_camera.pixelWidth * _camera.pixelHeight : height) / 2;
+    }
+}
