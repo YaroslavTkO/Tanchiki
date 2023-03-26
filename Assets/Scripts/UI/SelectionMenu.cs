039af3965c8c132b3daa5b73bb4bc12b6f15a476
YaroslavTk <yaroscro@gmail.com>
2023-03-26 19:55:23 +0300

    added changing color for selected buttons in selection menu

--- Assets/Scripts/UI/SelectionMenu.cs
+++ Assets/Scripts/UI/SelectionMenu.cs
@@ -14,9 +14,11 @@ public class SelectionMenu : MonoBehaviour
 
     private GameObject maps;
     private GameObject modes;
+    private Image selectedMapImage, selectedModeImage;
+    private Color selectedColor = new(0.2f, 0.8f, 0.2f, 1);
 
     private GameObject MapsPrefab { get { return Resources.Load("SelectionMenu/Maps") as GameObject; } }
-    private GameObject ModesPrefab { get { return Resources.Load("SelectionMenu/Modes") as GameObject; } }  
+    private GameObject ModesPrefab { get { return Resources.Load("SelectionMenu/Modes") as GameObject; } }
     private GameObject MapCardPrefab { get { return Resources.Load("SelectionMenu/MapUiPrefab") as GameObject; } }
     private GameObject ModeButtonPrefab { get { return Resources.Load("SelectionMenu/GameModeButton") as GameObject; } }
     private GameObject PlayButtonPrefab { get { return Resources.Load("SelectionMenu/PlayButton") as GameObject; } }
@@ -28,11 +30,11 @@ public class SelectionMenu : MonoBehaviour
         selectedData.gameMode = "";
         selectedData.mapName = "";
 
-        foreach(var map in data.Maps)
+        foreach (var map in data.Maps)
         {
             InstantiateMapCard(map);
         }
-        foreach(var mode in data.GameModes)
+        foreach (var mode in data.GameModes)
         {
             InstantiateModeButton(mode);
         }
@@ -45,7 +47,7 @@ public class SelectionMenu : MonoBehaviour
         var obj = Instantiate(MapCardPrefab, maps.transform);
         obj.GetComponentInChildren<Image>().sprite = map.SpriteForMenu;
         obj.GetComponentInChildren<TextMeshProUGUI>().text = map.Name;
-        obj.GetComponent<Button>().onClick.AddListener(()=>ChooseMap(map.Name));
+        obj.GetComponent<Button>().onClick.AddListener(() => ChooseMap(map.Name, obj));
 
     }
 
@@ -53,22 +55,32 @@ public class SelectionMenu : MonoBehaviour
     {
         GameObject obj = Instantiate(ModeButtonPrefab, modes.transform);
         obj.GetComponentInChildren<TextMeshProUGUI>().text = gamemodeName;
-        obj.GetComponent<Button>().onClick.AddListener(()=>ChooseMode(gamemodeName));
+        obj.GetComponent<Button>().onClick.AddListener(() => ChooseMode(gamemodeName, obj));
 
     }
 
-    private void ChooseMap(string mapName)
+    private void ChooseMap(string mapName, GameObject caller)
     {
         selectedData.mapName = mapName;
+
+        if (selectedMapImage != null)
+            selectedMapImage.color = Color.black;
+        selectedMapImage = caller.GetComponent<Image>();
+        selectedMapImage.color = selectedColor;
+
     }
-    private void ChooseMode(string modeName)
+    private void ChooseMode(string modeName, GameObject caller)
     {
         selectedData.gameMode = modeName;
+        if (selectedModeImage != null)
+            selectedModeImage.color = Color.white;
+        selectedModeImage = caller.GetComponent<Image>();
+        selectedModeImage.color = selectedColor;
     }
 
     private void Play()
     {
-        if(selectedData.mapName != "")
+        if (selectedData.mapName != "")
             SceneManager.LoadScene(selectedData.mapName);
     }
 }
